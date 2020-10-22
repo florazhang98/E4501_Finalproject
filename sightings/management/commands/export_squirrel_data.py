f54861c3279bf679f959a6987c7fe313f2081a9c
Jessica Lou <jl5776@columbia.edu>
2020-10-22 04:58:15 +0000

    Made some minor changes

--- sightings/management/commands/export_squirrel_data.py
+++ sightings/management/commands/export_squirrel_data.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
 from sightings.models import Squirrel
 from django.apps import apps
+
 import csv
+
 
 class Command(BaseCommand):
     help = 'exports all information to csv files'
@@ -12,13 +14,13 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         with open(kwargs['path'], 'w', newline='') as csvfile:
             attributes = ['Latitude', 
-            		 	'Longitude', 
-            			'Unique_Squirrel_ID', 
-            			'Shift', 
-            			'Date', 
-            			'Age', 
-            			'Primary_Fur_Color', 
-            			'Location',
+            		 'Longitude', 
+            		'Unique_Squirrel_ID', 
+            		'Shift', 
+            		'Date', 
+            		'Age', 
+            		'Primary_Fur_Color', 
+            		'Location',
                         'Specific_Location',
                         'Running',
                         'Chasing',
